d2c369eab3608a40208b31ecdfc95f373214c2ac
Francisco Cannepa <33263929+FranCannepa@users.noreply.github.com>
2022-06-16 18:08:03 -0300

    Update Obligatorio.java

--- src/trabajarconobligatorio/Obligatorio.java
+++ src/trabajarconobligatorio/Obligatorio.java
@@ -497,6 +497,7 @@ public class Obligatorio implements IObligatorio {
                     }
 
                     nodoLinea = nodoLinea.getSiguiente();
+                    contadorLinea++;
 
                 }
 
